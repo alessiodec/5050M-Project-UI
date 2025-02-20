d2dd03b2ff392498ae5ca228f3852814d2c7c715
alessiodec <alessiodec03@gmail.com>
2025-02-20 16:20:59 +0000

    Update load_models.py

--- functions/load_models.py
+++ functions/load_models.py
@@ -2,7 +2,7 @@ from tensorflow.keras.models import load_model
 
 def load_models():
     # Load the models
-    cr_model = load_model("CorrosionRateModel.keras")
-    sr_model = load_model("SaturationRateModel.keras")
+    cr_model = load_model("models/CorrosionRateModel.keras")
+    sr_model = load_model("models/SaturationRateModel.keras")
     
     return cr_model, sr_model
