f739fa5ec357c49cf3c5143fd26c5d82ad04aab5
alessiodec <alessiodec03@gmail.com>
2025-02-21 14:08:01 +0000

    Update input_histogram.py

--- functions/input_histogram.py
+++ functions/input_histogram.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
+import streamlit as st
 
 def input_histogram(df_subset):
-  # Use .iloc to subset columns 1-5
-  df_subset.iloc[:, 0:5].hist(bins=30, figsize=(12, 8))
-  plt.suptitle("Histograms of Inputs", y=0.95)
-  plt.tight_layout()
-  plt.show()
+    # Use .iloc to subset columns 1-5
+    df_subset.iloc[:, 0:5].hist(bins=30, figsize=(12, 8))
+    plt.suptitle("Histograms of Inputs", y=0.95)
+    plt.tight_layout()
+
+    # Display the plot in Streamlit
+    st.pyplot(plt)
