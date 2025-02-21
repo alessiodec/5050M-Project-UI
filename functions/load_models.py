0bfe8cb2fd7b22011e608ec28545ff688efb487a
alessiodec <alessiodec03@gmail.com>
2025-02-21 14:35:51 +0000

    Update load_models.py

--- functions/load_models.py
+++ functions/load_models.py
@@ -1,7 +1,7 @@
 from tensorflow.keras.models import load_model
 
 def load_models():
-    # Load the models
+    # Load the models (ensure the model files exist at these paths)
     cr_model = load_model("models/CorrosionRateModel.keras")
     sr_model = load_model("models/SaturationRateModel.keras")
     
