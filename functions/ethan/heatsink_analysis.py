8750ed4e16589032d9971ca0e60d54209c467d4b
alessiodec <alessiodec03@gmail.com>
2025-02-25 12:56:01 +0000

    Update heatsink_analysis.py

--- functions/ethan/heatsink_analysis.py
+++ functions/ethan/heatsink_analysis.py
@@ -4,6 +4,7 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st  # Streamlit-compatible plotting
+import warnings
 
 def run_heatsink_analysis(pop_size, pop_retention):
     """
